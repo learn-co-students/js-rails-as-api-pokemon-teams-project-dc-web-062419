73f13d26d41f8f745438f56caf1a4eaf8472d143
github username <github email address>
2019-08-12 18:05:37 -0400

    Finished app 2.0

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -15,6 +15,62 @@ function fectchTrainers(){
 function renderTrainer(trainer){
     let main = document.querySelector("main")
     let div = document.createElement("div")
-    div.innerHTML = `<div class="card" data-id='${trainer.id}'><p>${trainer.name}</p></div>`
+    div.innerHTML = `<div class="card" data-id='${trainer.id}'><p>${trainer.name}</p>
+        <button data-trainer-id="${trainer.id}">Add Pokemon</button>    
+        <ul id="pokemons "></ul>
+        </div>`
+    let addButton = div.querySelector("button")   
+    addButton.addEventListener("click", addPokemon) 
     main.appendChild(div)
+    let pokemonArray = trainer.pokemons
+    pokemonArray.forEach(renderPokemon)
+
+    function renderPokemon(pokemon){
+        let li = document.createElement("li")
+        let button = document.createElement("button")
+        button.classList.add("release")
+        button.dataset.pokemon_id=`${pokemon.id}`
+        button.innerText= "Release"
+        button.addEventListener("click", releasePokemon)
+        let list = div.querySelector("ul")
+        li.innerText= `${pokemon.nickname} (${pokemon.species})`
+        li.appendChild(button)
+        
+        list.appendChild(li)
+
+    }
+
+    function releasePokemon(event){
+        let id = event.target.dataset.pokemon_id
+        fetch(`http://localhost:3000/pokemons/${id}`, {
+            method: "DELETE"
+        }).then(response => response.json())
+        .then(releasedPokemon => {
+            event.target.parentElement.remove()
+            console.log("remove?", releasedPokemon)
+        })
+    }
+
+    function addPokemon(event) {
+        // debugger
+       let data = {
+           trainer_id: event.target.dataset.trainerId,
+        }
+        fetch("http://localhost:3000/pokemons", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        // .then(response => console.log(response))
+        .then(data => {
+            if (data['message']) 
+                alert("TOO MANNNNNNNYYYY")
+            else
+                renderPokemon(data)
+            end }
+            )
+    }
 }
