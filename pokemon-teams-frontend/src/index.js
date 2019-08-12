efb34d6e0098ec4a49142ebba08c8aacfd931651
github username <github email address>
2019-08-12 15:07:16 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,52 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    thoseThatCatchThemAll()
+    gottaCatchEmAll()
+})
+
+
+
+
+function thoseThatCatchThemAll(){
+    fetch(`${TRAINERS_URL}`)
+    .then(response => response.json())
+    .then(trainersArray => trainersArray.forEach(renderTrainers))
+   
+}
+function renderTrainers(trainer){
+ let main = document.getElementById("main")
+ let card = document.createElement("div")
+ let pokeButton = document.createElement("button")
+ main.appendChild(card)
+ card.appendChild(pokeButton)
+ pokeButton.classList.add(`button-${trainer.id}`)
+ card.classList.add(`card-${trainer.id}`)
+ card.innerText = trainer.name
+ pokeButton.innerText = "Add Pokemon"
+}
+
+function gottaCatchEmAll(){
+    fetch(`${POKEMONS_URL}`)
+    .then(response => response.json())
+    .then(pokemonArray => pokemonArray.forEach(renderPokemon))
+    }
+    
+    function renderPokemon(pokemon){
+        let card = document.querySelector("div")
+        let list = document.createElement("ul")
+        let listItem = document.createElement("li")
+        let button = ocument.createElement("button")
+        card.appendChild(list)
+        list.appendChild(listItem)
+        listItem.insertAdjacentHTML("beforeend", ` ${button}`)
+    }
+
+
+
+
+
+
