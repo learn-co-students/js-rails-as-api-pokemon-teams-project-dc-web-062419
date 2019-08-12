470eac001736d38d54dd4fd09b82440b6390349b
github username <github email address>
2019-08-12 18:05:03 -0400

    Finished app

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,16 +1,32 @@
 class PokemonsController < ApplicationController
-    
+
 
     def index
         render json:  Pokemon.all.to_json(pokemon_serializer_options)
     end
 
     def create
-        # byebug
-        pokemon = Pokemon.create(
-            nickname: Faker::Name.first_name,
-            species: Faker::Games::Pokemon.name,
-            trainer_id: params["trainer_id"].to_i)
+        #  byebug
+        trainer_id = params["trainer_id"].to_i
+        if (Trainer.find(trainer_id).pokemons.count < 6)
+            pokemon = Pokemon.create(
+                nickname: Faker::Name.first_name,
+                species: Faker::Games::Pokemon.name,
+                trainer_id: params["trainer_id"].to_i)
+            render json: pokemon 
+        else 
+            # alert("Too many Pokemon, don't be greedy")
+            render json: {
+                message: "error"
+            }
+        end   
+        
+    end
+
+    def destroy
+        pokemon = Pokemon.find(params[:id])
+        pokemon.destroy
+        render json: pokemon
     end
 
     private
