6f1d82c34eb690c805313340ca0f3ad5391d5a53
claudiasolbes <cssolbes@hotmail.com>
2019-08-12 16:22:04 -0400

    built trainer cards

--- pokemon-teams-frontend/src/trainers.js
+++ pokemon-teams-frontend/src/trainers.js
@@ -6,6 +6,34 @@ function fetchTrainers(){
 
 }
 
-function renderTrainer(){
+function renderTrainer(trainer){
+    const trainerDiv = document.createElement("div");
+    trainerDiv.classList.add("card");
+    console.log(trainerDiv)
+    const main = document.querySelector("main")
+    main.appendChild(trainerDiv)
+    const trainerP = document.createElement("p")
+    trainerDiv.appendChild(trainerP)
+    trainerP.innerText = trainer.name
+    const addButton = document.createElement("button")
+    trainerDiv.appendChild(addButton)
+    addButton.innerText = "Add Pokemon"
 
+    //ul for trainers pokemons
+    const trainerUl = document.createElement("ul")
+    trainerDiv.appendChild(trainerUl)
+    trainer.pokemons.forEach( function(pokemon){
+        renderPokemons(pokemon, trainerUl)
+    })
 }
+
+function renderPokemons(pokemon, trainerUl){
+    const pokemonLi = document.createElement("li")
+    trainerUl.appendChild(pokemonLi)
+    pokemonLi.innerText = `${pokemon.nickname} (${pokemon.species})`
+    const releaseButton = document.createElement("button")
+    pokemonLi.appendChild(releaseButton)
+    releaseButton.classList.add("release")
+    releaseButton.innerText = "Release"
+}
+
