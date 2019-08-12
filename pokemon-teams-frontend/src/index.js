405134ab7a0ca5c9931a1853586b92a8d8ada0b3
ruggz13 <nicolasruggieri7@gmail.com>
2019-08-12 15:05:25 -0400

    created cards with p tag

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,20 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", function(){
+    fectchTrainers()
+})
+
+function fectchTrainers(){
+    fetch("http://localhost:3000/trainers")
+    .then(response => response.json())
+    .then(trainerArray => trainerArray.forEach(renderTrainer))
+}
+
+function renderTrainer(trainer){
+    let main = document.querySelector("main")
+    let div = document.createElement("div")
+    div.innerHTML = `<div class="card" data-id='${trainer.id}'><p>${trainer.name}</p></div>`
+    main.appendChild(div)
+}
